a794e85035f1d84e0b13b9fcc9ee1fbefde62502
ANNotunzdY <webmaster@smilemac.com>
2017-05-22 02:46:16 +0900

    X-Forwarded-For Header Support

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-if (preg_match('/^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/', $_SERVER['REMOTE_ADDR'])) {
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$headers = apache_request_headers();
+if ($headers['X-Forwarded-For']) {
+	$ip = $headers['X-Forwarded-For'];
+}
+
+if (preg_match('/^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/', $ip)) {
 	echo '<div style="background: linear-gradient(#FF0000, #FF99CC); padding: 5px; color: white; border: 1px solid #333333; border-radius: 3px; font-size: 13px; width: 50px; text-align: center; font-family: sans-serif;">via IPv4';
 } else {
 	echo '<div style="background: linear-gradient(#0000FF, #99CCFF); padding: 5px; color: white; border: 1px solid #333333; border-radius: 3px; font-size: 13px; width: 50px; text-align: center; font-family: sans-serif;">via IPv6';
